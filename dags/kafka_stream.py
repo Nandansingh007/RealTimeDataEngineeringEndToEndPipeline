cf900d2f2b18e453e358415a9571f683fa9946b3
Roji Kumari Singh <nandan.singhs007@gmail.com>
2024-04-16 15:37:05 +0545

    Settin up Kafka Producer

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -48,12 +48,17 @@ def format_data(res):
 
 def stream_data():
     import json
+    from kafka import KafkaProducer
     
     res = get_data()
 
     res = format_data(res)
 
-    print(json.dumps(res, indent=3))
+    # print(json.dumps(res, indent=3))
+
+    producer = KafkaProducer(bootstrap_servers=['localhost:9092'],max_block_ms=5000)
+
+    producer.send('user_created',json.dumps(res).encode('utf=8'))
     
 
 stream_data()
